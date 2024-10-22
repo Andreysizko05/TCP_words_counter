3b754abd274ee925b29604981a7240f7988cc589
SyzkoAndrew <andrey.s@techspark.engineering>
2024-10-22 22:32:25 +0300

    Added to client and server text files sending and recieving

--- tcpClient/src/main.cpp
+++ tcpClient/src/main.cpp
@@ -2,15 +2,17 @@
 #include <QTcpSocket>
 #include <QHostAddress>
 #include <QDebug>
+#include <QFile>
+#include <QDataStream>
 #include <iostream>
 
 int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
 
-    if (argc != 2)
+    if (argc != 3)
     {
-        std::cerr << "Usage: client <host>" << std::endl;
+        std::cerr << "Usage: client <host> <filepath>" << std::endl;
         return 1;
     }
     QString host = argv[1]; // Get the host from arguments
@@ -29,23 +31,61 @@ int main(int argc, char* argv[])
     }
     qDebug() << "Connection established";
 
-    // Buffer for reading data
-    QByteArray data;
+    std::string input(argv[2]);
+    QString fullpath;
+    std::string toRemove = "programBinDir";  // Substring to remove
+    size_t firstSlashPos = input.find('/');
 
-    // Wait for data from the server
-    while (socket.waitForReadyRead(5000))
-    { // Wait for data (no more than 5 seconds)
-        data.append(socket.readAll()); // Read all available data
+    if (firstSlashPos != std::string::npos && input.substr(0, firstSlashPos) == toRemove)
+    {
+        input.erase(0, firstSlashPos);
+        fullpath = QCoreApplication::applicationDirPath() + QString::fromStdString(input);
+        qDebug() << "string: \"programBinDir\" has been found";
+        qDebug() << "Resulting path: " << fullpath;
+    }
+    else
+    {
+        fullpath = QString::fromStdString(input);
+        qDebug() << "Resulting path: " << fullpath;
     }
 
-    if (data.isEmpty())
+    QFile file(fullpath);
+    if (!file.open(QIODevice::ReadOnly)) {
+        qDebug() << "Could not open file";
+        return 1;
+    }
+
+    QDataStream stream(&socket);
+    QByteArray fileData = file.readAll();
+    qint64  fileSize = file.size();
+
+    // Send file size
+    QByteArray fileSizeData = QString::number(file.size()).toUtf8();
+    socket.write(fileSizeData);
+    socket.write("\n");
+
+    // Send file content
+    qDebug() << "File content:\n" << fileData;
+    stream.writeRawData(fileData.data(), fileData.size());  // Send the file content
+
+    socket.flush();
+    socket.waitForBytesWritten(3000);
+    qDebug() << "File sent successfully.";
+
+    // Wait for response from server
+    QByteArray responseData;
+    while (socket.waitForReadyRead(5000))
     {
+        responseData.append(socket.readAll());
+    }
+
+    if (responseData.isEmpty()) {
         qDebug() << "No data received.";
         return 1;
     }
 
     // Output the received data
-    qDebug() << data.constData();
+    qDebug() << "Received from server: " << responseData.constData();
 
     // Close the connection
     socket.disconnectFromHost();
@@ -53,5 +93,5 @@ int main(int argc, char* argv[])
         socket.waitForDisconnected();
 
     return 0; // For GUI Apps
-    //return a.exec();
+	//return a.exec();
 }
--- tcpServer/src/main.cpp
+++ tcpServer/src/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <string>
 #include <boost/asio.hpp>
-#include <iostream>
+#include <boost/array.hpp>
+#include <fstream>
 
 using boost::asio::ip::tcp;
 
@@ -10,6 +11,56 @@ std::string make_answer_string()
     static int reqCounter = 0;
     std::cout << "server answers count = " << ++reqCounter << std::endl;
     return "Hell " + std::to_string(reqCounter);
+}
+
+std::string read_until_delimiter(tcp::socket& socket, char delimiter)
+{
+    std::string result;
+    char buffer;
+
+    while (true) {
+        boost::asio::read(socket, boost::asio::buffer(&buffer, 1));
+        if (buffer == delimiter) {
+            break;
+        }
+        result += buffer;
+    }
+
+    return result;
+}
+void receive_file(tcp::socket& socket)
+{
+    try {
+        // Step 1: Read file size until the newline character
+        std::string fileSizeStr = read_until_delimiter(socket, '\n');
+        std::uint64_t fileSize = std::stoull(fileSizeStr);
+        std::cout << "File size: " << fileSize << " bytes" << std::endl;
+
+        // Step 2: Read file data in chunks
+        std::vector<char> fileData;
+        fileData.reserve(fileSize); // Reserve space for the file
+
+        std::size_t bytesReceived = 0;
+        while (bytesReceived < fileSize)
+        {
+            std::vector<char> chunk(1024); // Reading in chunks of 1024 bytes
+            std::size_t bytesToRead = std::min(static_cast<std::size_t>(fileSize - bytesReceived), chunk.size());
+
+            std::size_t bytesRead = socket.read_some(boost::asio::buffer(chunk, bytesToRead));
+            fileData.insert(fileData.end(), chunk.begin(), chunk.begin() + bytesRead);
+            bytesReceived += bytesRead;
+
+            std::cout << "Received " << bytesReceived << " of " << fileSize << " bytes" << std::endl;
+        }
+
+        std::cout << "File successfully received:" << std::endl;
+        for (char c : fileData)
+            std::cout << c;
+        std::cout << std::endl;
+    }
+    catch (std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
 }
 
 int main()
@@ -26,8 +77,9 @@ int main()
             tcp::socket socket(io_context);
             acceptor.accept(socket);
 
-            std::string message = make_answer_string();
+            receive_file(socket);
 
+            std::string message = make_answer_string();
             boost::system::error_code ignored_error;
             boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
         }
